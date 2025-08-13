db901852d36b5df677e8eec8801b2a9e2c1c7d5e
jpig <isdolinskayaym@gmail.com>
2025-08-13 21:07:30 +0200

    implemented parsing pairs into AST

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -106,95 +106,95 @@ struct DOMParser;
 pub fn parse_html(input: &str) -> Result<Pair<'_, Rule>, Error<Rule>> {
 
     let dom = DOMParser::parse(Rule::document, input)?.next().unwrap();
-    
-    // let ast = elem("body".to_string(),
-    //             {
-    //                 let mut attrs = HashMap::new();
-    //                 attrs
-    //             }, Children::Nodes(
-    //                 vec![
-    //                     match pair.as_rule() {
-    //                         Rule::heading => heading(2, "".to_string()),
-    //                         _ => todo!()
-    //                     }
-    //             ]
-    //             ));
+
     Ok(dom)
 }
 
-pub fn parse_nodes(pair: Pair<Rule>) -> Option<Node> {
+pub fn parse_nodes(pair: Pair<Rule>) -> Node {
     match pair.as_rule() {
         Rule::document => {
-                // let children =  pair.into_inner().skip(2).next();
-
-                //  let test: Vec<Pair<'_, Rule>> = pair.into_inner().next().unwrap().into_inner().skip(2).collect();
-
-                let body =  parse_nodes(pair.into_inner().next().unwrap()).unwrap();
-                
-                // let children: Vec<_> = pair.into_inner().map(|pair| {
-                //     pair.into_inner().skip(2).next()
-                // }).collect();
-
-                // print!("--------- DEBUG----------- {:#?}", children);
-                Some(elem(
+                let body =  parse_nodes(pair.into_inner().next().unwrap());
+                let result = elem(
                     "body".to_string(),
-                    {
-                            let mut attrs = HashMap::new();
-                            attrs
-                        },
+                    HashMap::new(),
                         Children::Node(Box::new(body))
 
-                ))
+                );
+
+                result
         },
         Rule::element => {
                 let mut inner = pair.into_inner();
                 let tag_name = inner.next().unwrap().as_str().to_string();
 
-                fn parse_attrs(pairs: Option<Pair<Rule>>) -> AttrMap {
+                fn parse_attrs(inner_pair: Pair<Rule>) -> AttrMap {
                       let mut attrs: AttrMap = HashMap::new();
 
-                      pairs.map(|pair| {
-                        let mut inner = pair.into_inner();
+                      if inner_pair.as_rule() == Rule::tag_attr {
+                            let mut inner_inner = inner_pair.into_inner();
+                            let name = inner_inner.next().unwrap().as_str();
+                            let value = inner_inner.next().unwrap().as_str();
 
-                        let name = inner.next().unwrap().as_str();
-                        let value = inner.next().unwrap().as_str();
-
-                        attrs.insert(name.to_string(), value.to_string());
-                    });
+                            attrs.insert(name.to_string(), value.to_string());
+                      }
 
                     attrs
                 }
-                let attrs =  parse_attrs(inner.clone().next());
                 
-                let children: Vec<Node> = inner.skip(1).next().unwrap().into_inner().map(|child| {
-                    parse_nodes(child).unwrap()
+                let attrs =  parse_attrs(inner.clone().next().unwrap());
+                
+                let children_iter = if attrs.is_empty() {
+                    inner.skip(0)
+                } else {
+                    inner.skip(1)
+                };
+                
+                let children: Vec<Node> = children_iter.into_iter().map(|child| {
+                    parse_nodes(child)
                 }).collect();
 
-                println!("----------------> ELEMENT DATA is: \n tag_name {:#?};\n attrs {:#?};\n children {:#?}", tag_name, attrs, children);
-
-                Some(elem(tag_name, attrs, Children::Nodes(children)))
+                elem(tag_name, attrs,  Children::Nodes(children))
         },
-        Rule::children => Some(parse_nodes(pair.into_inner().next()?).unwrap()),
+        Rule::children => {
+            parse_nodes(pair.into_inner().next().unwrap())
+        },
         Rule::heading => {
             let mut inner = pair.into_inner();
 
             let heading_level: u8 = inner.next().unwrap().as_str().parse().unwrap();
             let content = inner.next().unwrap().as_str().to_string();
 
-            Some(heading(heading_level, content))
+            heading(heading_level, content)
         },
         Rule::comment => {
-            Some(comment(pair.as_str().to_string()))
+            comment(pair.as_str().to_string())
         },
         Rule::description => {
-            parse_nodes(pair.into_inner().next().unwrap())
+            desc(pair.into_inner().next().unwrap().as_str().to_string())
         },
         Rule::text_content => {
-            Some(text(pair.as_str().to_string()))
+            text(pair.as_str().to_string())
         },
-        _ => {
-            println!("-------- None of the PAIR------ {:#?}", pair);
-            None
-        }
+        Rule::WHITESPACE
+        | Rule::_opening_tag
+        | Rule::_closing_tag
+        | Rule::_QUOTE
+        | Rule::_comment_block
+        | Rule::_comment_start
+        | Rule::_comment_end
+        | Rule::heading_level
+        | Rule::_heading_opening
+        | Rule::_heading_closing
+        | Rule::_left_chevron
+        | Rule::_right_chevron
+        | Rule::tag_name
+        | Rule::tag_attr
+        | Rule::attr_name
+        | Rule::attr_value
+        | Rule::_description_opening
+        | Rule::_description_closing
+       | Rule::eoi => {
+        unreachable!()
+       }
     }
 }
--- tests/parser.rs
+++ tests/parser.rs
@@ -1,9 +1,9 @@
 #[cfg(test)]
 mod tests {
-    use wave::parser::*;    
     use pest_ascii_tree::print_ascii_tree;
     use pretty_assertions::assert_eq;
     use std::collections::HashMap;
+    use wave::parser::*;
 
     #[test]
     fn test_parse_html() {
@@ -31,61 +31,66 @@ mod tests {
                 </div>
             </div>
         "#;
+
         let expected_result = elem(
-        "div".to_string(),
-        {
-            let mut attrs = HashMap::new();
-            attrs.insert("class".to_string(), "root".to_string());
-            attrs
-        },
-        Children::Nodes(vec![
-            heading(1, "Hello, world!".to_string()),
-            comment("Container with items".to_string()),
-            elem(
+            "body".to_string(),
+            HashMap::new(),
+            Children::Node(Box::new(elem(
                 "div".to_string(),
                 {
                     let mut attrs = HashMap::new();
-                    attrs.insert("class".to_string(), "container".to_string());
+                    attrs.insert("class".to_string(), "root".to_string());
                     attrs
                 },
                 Children::Nodes(vec![
+                    heading(1, "Hello, world!".to_string()),
+                    comment("Container with items".to_string()),
                     elem(
                         "div".to_string(),
                         {
                             let mut attrs = HashMap::new();
-                            attrs.insert("class".to_string(), "item item1".to_string());
-                            attrs
-                        },
-                            Children::Nodes(vec![
-                                heading(2, "Foo".to_string()),
-                                comment("This is a\nmulti-line comment.".to_string()),
-                                desc("This is very nice item.".to_string()),
-                                text("It costs 100$".to_string())
-                            ])
-                    ),
-                    elem(
-                        "div".to_string(),
-                        {
-                            let mut attrs = HashMap::new();
-                            attrs.insert("class".to_string(), "item item2".to_string());
+                            attrs.insert("class".to_string(), "container".to_string());
                             attrs
                         },
                         Children::Nodes(vec![
-                            heading(2, "Bar".to_string()),
-                            desc("This is small item".to_string()),
-                            text("It costs 20$".to_string())
-                        ])
-                    )
-                ])
-            )
-        ])
-    );
+                            elem(
+                                "div".to_string(),
+                                {
+                                    let mut attrs = HashMap::new();
+                                    attrs.insert("class".to_string(), "item item1".to_string());
+                                    attrs
+                                },
+                                Children::Nodes(vec![
+                                    heading(2, "Foo".to_string()),
+                                    comment("This is a\n                        multi-line comment.".to_string()),
+                                    desc("This is very nice item.".to_string()),
+                                    text("It costs 100$".to_string()),
+                                ]),
+                            ),
+                            elem(
+                                "div".to_string(),
+                                {
+                                    let mut attrs = HashMap::new();
+                                    attrs.insert("class".to_string(), "item item2".to_string());
+                                    attrs
+                                },
+                                Children::Nodes(vec![
+                                    heading(2, "Bar".to_string()),
+                                    desc("This is small item".to_string()),
+                                    text("It costs 20$".to_string()),
+                                ]),
+                            ),
+                        ]),
+                    ),
+                ]),
+            ))),
+        );
 
         let dom = parse_html(input).unwrap();
 
         print_ascii_tree(Ok(dom.clone().into_inner()));
 
-        let result = parse_nodes(dom).unwrap();
+        let result = parse_nodes(dom);
 
         assert_eq!(&result, &expected_result);
     }
